309f22edeb48d2bc77b284e47322d329f2f0f56b
Chen Reuven <chen.reuven@cyberark.com>
2024-01-24 11:45:15 +0200

    init

--- index.js
+++ index.js
@@ -1,61 +1,37 @@
-#! /usr/bin/env node
-
 const { Command } = require("commander");
-const fs = require("fs");
-const path = require("path");
+const program = new Command();
 const figlet = require("figlet");
 
-const program = new Command();
-
-console.log(figlet.textSync("Chen CLI"));
+console.log(figlet.textSync("String Cli"));
 
 program
-  .version("1.0.0")
-  .description("An example CLI for managing a directory")
-  .option("-l, --ls  [value]", "List directory contents")
-  .option("-m, --mkdir <value>", "Create a directory")
-  .option("-t, --touch <value>", "Create a file")
-  .parse(process.argv);
+  .name('string-util')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
 
-const options = program.opts();
+program.command('split')
+  .description('Split a string into substrings and display as an array.')
+  .argument('<string>', 'string to split')
+  .option('--first', 'display just the first substring')
+  .option('-s, --separator <char>', 'separator character', ',')
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
 
-async function listDirContents(filepath) {
-  try {
-    const files = await fs.promises.readdir(filepath);
-    const detailedFilesPromises = files.map(async (file) => {
-      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
-      const { size, birthtime } = fileDetails;
-      return { filename: file, "size(KB)": size, created_at: birthtime };
-    });
-    const detailedFiles = await Promise.all(detailedFilesPromises);
-    console.table(detailedFiles);
-  } catch (error) {
-    console.error("Error occurred while reading the directory!", error);
-  }
-}
-function createDir(filepath) {
-  if (!fs.existsSync(filepath)) {
-    fs.mkdirSync(filepath);
-    console.log("The directory has been created successfully");
-  }
-}
+program.command('join')
+  .description('Join the command-arguments into a single string')
+  .argument('<strings...>', 'one or more strings')
+  .option('-s, --separator <char>', 'separator character', ',')
+  .action((strings, options) => {
+    console.log(strings.join(options.separator));
+  });
 
-function createFile(filepath) {
-  fs.openSync(filepath, "w");
-  console.log("An empty file has been created");
-}
+program.parse();
 
-if (options.ls) {
-  const filepath = typeof options.ls === "string" ? options.ls : __dirname;
-  listDirContents(filepath);
-}
-if (options.mkdir) {
-  createDir(path.resolve(__dirname, options.mkdir));
-}
-if (options.touch) {
-  createFile(path.resolve(__dirname, options.touch));
-}
-
-// if (!process.argv.slice(2).length) {
-//     program.outputHelp();
-//   }
+// Try the following:
+//    node string-util
+//    node string-util help split
+//    node string-util split --separator=/ a/b/c
+//    node string-util split --first a,b,c
+//    node string-util join a b c d
